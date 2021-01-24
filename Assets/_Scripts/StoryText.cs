eb1fcf6c09fb2d857bcb08341c0610223b4b8771
MikeR <mikeraadsheer11@gmail.com>
2021-01-24 22:17:08 +0100

    Created Main story dialogue and scrolling text.

---
+++ Assets/_Scripts/StoryText.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class StoryText : MonoBehaviour
+{
+    
+    public Text txt;
+    public float scrollSpeed = 10f;
+    public float loadPoint; // value for top one-minus.
+    public int LevelID;
+
+    private TextMeshProUGUI _TMTxt;
+    private RectTransform _trans;
+
+    void Start()
+    {
+        _trans = GetComponent<RectTransform>();
+        
+        _TMTxt = GetComponent<TextMeshProUGUI>();
+        _TMTxt.text = txt.text;
+    }
+
+    void Update()
+    {
+        _trans.offsetMax += new Vector2(0f, scrollSpeed * Time.deltaTime);
+
+        if(_trans.offsetMax.y > loadPoint)
+        {
+            SceneManager.LoadScene(LevelID);
+        }
+    }
+}
