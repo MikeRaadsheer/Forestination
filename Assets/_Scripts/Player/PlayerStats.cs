2850e081f90ff24b6a5f73c16b91b89b58ec5530
MikeR <mikeraadsheer11@gmail.com>
2021-01-25 20:18:22 +0100

    Added basic player stats and movement with stamina system

---
+++ Assets/_Scripts/Player/PlayerStats.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System;
+
+public class PlayerStats : MonoBehaviour
+{
+    
+    private static int MaxHP = 100;
+    private static int HP;
+    private static float MaxStamina = 25;
+    private static float Stamina;
+    public static float Noise = 0f;
+    public static Action GameOver;
+
+    public static float ElapsedTime;
+
+    private void Start()
+    {
+        HP = MaxHP;
+        Stamina = MaxStamina;
+    }
+
+    private void FixedUpdate()
+    {
+        ElapsedTime += Time.fixedDeltaTime;
+    }
+
+    public static void TakeDamage(int amount)
+    {
+        HP -= amount;
+
+        if (HP < 0)
+        {
+            HP = 0;
+        }
+    }
+
+    public static void Heal(int amount)
+    {
+        HP += amount;
+
+        if (HP > MaxHP)
+        {
+            HP = MaxHP;
+        }
+    }
+
+    public static void DrainStamina(float amount)
+    {
+        Stamina -= amount;
+
+        if (Stamina < 0)
+        {
+            Stamina = 0;
+        }
+    }
+
+    public static void RestoreStamina(float amount)
+    {
+        Stamina += amount;
+
+        if (Stamina > MaxStamina)
+        {
+            Stamina = MaxStamina;
+        }
+    }
+
+    public static void AddNoise(float amount)
+    {
+        Noise += amount;
+    }
+
+    public static void RemoveNoise(float amount)
+    {
+        Noise -= amount;
+    }
+
+    public static float GetNoise()
+    {
+        return Noise;
+    }
+
+    public static int GetHP()
+    {
+        return HP;
+    }
+
+    public static float GetStamina()
+    {
+        return Stamina;
+    }
+
+    public static void EndGame()
+    {
+        Debug.Log("Game Over");
+        GameOver();
+    }
+
+}
